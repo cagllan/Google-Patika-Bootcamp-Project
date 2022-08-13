957c092f50d02550354a64ff0fef86afb3a096f0
Arda Ozkardesler <aozkardesler@gmail.com>
2022-08-13 14:36:59 +0300

    drag script disabled on fight

--- Assets/BattleSystem/Scripts/Button/FightButton.cs
+++ Assets/BattleSystem/Scripts/Button/FightButton.cs
@@ -4,6 +4,7 @@ public class FightButton : MonoBehaviour
 {
     [SerializeField] private GameObject  _fightPanel;
     [SerializeField] private Button _fightButton;
+    [SerializeField] Transform[] slots;
     
     private void Awake()
     {
@@ -14,6 +15,13 @@ public class FightButton : MonoBehaviour
     {
         SetFightPanelVisibilty(false);
         BattleStartController.Instance.StartBattle();
+        foreach (var slot in slots)
+        {
+            if (slot.childCount > 0)
+            {
+                Destroy(slot.GetChild(0).GetComponent<Drag>());
+            }
+        }
     }
 
     private void OnDestroy()
