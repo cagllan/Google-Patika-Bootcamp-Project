cf02d658648b56dd859c2983301bfff1a010171d
Caglan Atay <caglanatay@windowslive.com>
2022-08-13 17:07:07 +0300

    fixed win particle script

--- Assets/BattleSystem/Scripts/Particle/FriendlyWinParticle.cs
+++ Assets/BattleSystem/Scripts/Particle/FriendlyWinParticle.cs
@@ -4,19 +4,13 @@ public class FriendlyWinParticle : MonoBehaviour
 {
     [SerializeField] ParticleSystem _friendlyWinParticle = null;
 
-
     private void Awake() 
     {
         TeamVictoryControl.Instance.OnSoldierTeamWon += OnSoldierTeamWon;
     }
 
-    private void Start() {
-        gameObject.SetActive(false);
-    }
-
     private void OnDestroy() 
     {
-        gameObject.SetActive(false);
         if(TeamVictoryControl.Instance != null) TeamVictoryControl.Instance.OnSoldierTeamWon -= OnSoldierTeamWon;
     }
 
@@ -30,7 +24,6 @@ public class FriendlyWinParticle : MonoBehaviour
 
     private void StartParticle()
     {
-        gameObject.SetActive(true);
         _friendlyWinParticle.Play();        
     }
 }
